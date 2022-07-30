83fca0fa9ac951f18b274997703e3a91ab91b61e
Izumi Asakura <5370686+iasakura@users.noreply.github.com>
2022-07-31 01:59:14 +0900

    improved

--- components/KondateSolver.tsx
+++ components/KondateSolver.tsx
@@ -2,12 +2,15 @@ import type { NextPage } from "next";
 import React from "react";
 import { KondateTable } from "./KondateTable";
 import { computeKondate, computeTotal, type Kondate } from "../models/Kondate";
+import { add, format } from "date-fns";
+import { useLocalStorage } from ".";
 
 const useFoods = (props: {
   category: string;
   default: string;
+  key: string;
 }): [string[], React.ReactElement] => {
-  const [foods, setFoods] = React.useState(props.default);
+  const [foods, setFoods] = useLocalStorage(props.key, props.default);
   const handleChange = (ev: React.KeyboardEvent<HTMLInputElement>) => {
     ev.preventDefault();
     setFoods(ev.target.value);
@@ -33,73 +36,69 @@ const useFoods = (props: {
   return [parsedFoods, form];
 };
 
-const useWeekday = (): [React.ReactElement, string[]] => {
-  const weekdays = [
-    "月曜日",
-    "火曜日",
-    "水曜日",
-    "木曜日",
-    "金曜日",
-    "土曜日",
-    "日曜日",
-  ];
-  const doubleWeekdays = [...weekdays, ...weekdays];
+const formatDate = (d: Date) => format(d, "MM/dd (eee)");
 
-  const [startDay, setStartDay] = React.useState(0);
+const useStartDay = (): [React.ReactElement, string[]] => {
+  const [startDay, setStartDay] = React.useState(
+    format(new Date(), "yyyy-MM-dd")
+  );
 
-  const handleChange = (ev: React.ChangeEvent<HTMLSelectElement>) => {
-    ev.preventDefault();
-    const selected = weekdays.findIndex((day) => day === ev.target.value);
-    console.log(ev.target.value);
-    console.log(selected);
-    if (selected >= 0) {
-      setStartDay(selected);
-    }
-  };
+  const handleChange = React.useCallback(
+    (ev: React.ChangeEvent<HTMLInputElement>) => {
+      ev.preventDefault();
+      setStartDay(format(new Date(ev.target.value), "yyyy-MM-dd"));
+    },
+    []
+  );
+
+  const days = [...Array(10).keys()].map((i) =>
+    formatDate(add(new Date(startDay), { days: i }))
+  );
 
   const selector = (
     <label>
-      始まり曜日:
-      <select onChange={handleChange}>
-        {weekdays.map((day, idx) => (
-          <option key={day}>{day}</option>
-        ))}
-      </select>
+      始まり日:
+      <input type="date" value={startDay} onChange={handleChange} />
     </label>
   );
 
-  return [selector, doubleWeekdays.slice(startDay, startDay + 7)];
+  return [selector, days];
 };
 
 export const KondateSolver: NextPage = () => {
   const [carbo, carboForm] = useFoods({
     category: "炭水化物",
     default: "そうめん オートミール",
+    key: "carbo-eaten",
   });
   const [protein, proteinForm] = useFoods({
     category: "タンパク質",
     default: "豆腐 鯛 しらす",
+    key: "protein-eaten",
   });
   const [vitamin, vitaminForm] = useFoods({
     category: "ビタミン",
     default: "トマト かぶ キャベツ ほうれん草 にんじん りんご",
+    key: "vitamin-eaten",
   });
 
   const [newCarbo, newCarboForm] = useFoods({
     category: "炭水化物",
     default: "",
+    key: "carbo-new",
   });
   const [newProtein, newProteinForm] = useFoods({
     category: "タンパク質",
     default: "",
+    key: "protein-new",
   });
   const [newVitamin, newVitaminForm] = useFoods({
     category: "ビタミン",
     default: "",
+    key: "vitamin-new",
   });
 
-  const [weekdayForm, weekdays] = useWeekday();
-  console.log(weekdays);
+  const [weekdayForm, weekdays] = useStartDay();
 
   const [kondate, setKondate] = React.useState<Kondate | undefined>(undefined);
 
