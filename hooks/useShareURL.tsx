96ada3c3ebd54721cdbddaa9e86cb814c914f4fe
Izumi Asakura <5370686+iasakura@users.noreply.github.com>
2022-08-22 00:47:48 +0900

    fix

--- hooks/useShareURL.tsx
+++ hooks/useShareURL.tsx
@@ -3,8 +3,8 @@ import { shareUrl } from "../models/shareURL";
 import { Foods } from "./useFoods";
 
 export const useShareURLButton = (foods: Foods) => {
-  // const url = shareUrl("https://kondate.vercel.app/", foods);
-  const url = shareUrl("http://localhost:3000/", foods);
+  const url = shareUrl("https://kondate.vercel.app/", foods);
+  // const url = shareUrl("http://localhost:3000/", foods);
 
   const handleClick = useCallback(() => {
     navigator.clipboard.writeText(url);
